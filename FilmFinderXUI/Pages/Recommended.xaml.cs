73b6b346358060b63597a45a9bf322a0e97029a9
royclaudio <royclaudio4@gmail.com>
2023-01-02 22:01:55 -0700

    ui

---
+++ FilmFinderXUI/Pages/Recommended.xaml.cs
@@ -0,0 +1,27 @@
+﻿using FilmFinderXUI.DataServices;
+namespace FilmFinderXUI;
+
+public partial class Recommended : ContentPage
+{
+    private readonly IRestDataService _dataService;
+
+    public Recommended(IRestDataService dataService)
+    {
+		InitializeComponent();
+        _dataService = dataService;
+
+    }
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+        // movies = _dataService.GetMoviesAsync();
+    }
+    async void SavePopup(object sender, EventArgs e)
+    {
+        string action = await DisplayActionSheet("Save Movie", "Cancel", "Like", "Interested");
+    }
+
+    void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+    }
+}
